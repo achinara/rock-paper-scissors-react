658629fbd7d838ec41a0c92fdd6c1a2a2f6b9b70
Aitkulova <aitkulova.ch@gmail.com>
2018-08-05 17:51:57 +0300

    styles changes

--- src/components/Pointers/Pointers.js
+++ src/components/Pointers/Pointers.js
@@ -4,13 +4,13 @@ import './Pointers.css'
 const Pointers = ({result=''}) => {
   return(
     <div className='pointers'>
-      <div className='pointers__left'>Соперник</div>
+      <div className={`${result} pointers__left`}>Соперник</div>
       <div className='pointers__center'>
-        <i className={`${result === 'loss' && 'blue'} pointers__icon-arr-left`}/>
-        <i className={`${result === 'draw' && 'blue'} pointers__icon-x`}/>
-        <i className={`${result === 'win' && 'blue'} pointers__icon-arr-right`}/>
+        <i className={`${result} pointers__icon-arr-left`}/>
+        <i className={`${result} pointers__icon-x`}/>
+        <i className={`${result} pointers__icon-arr-right`}/>
       </div>
-      <div className='pointers__right'>Вы</div>
+      <div className={`${result} pointers__right`}>Вы</div>
     </div>
   )
 };
